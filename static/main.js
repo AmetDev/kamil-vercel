d01d089a4da2081467ee7ef981b2b93b56b1280d
AmetJunior <ametsejdaliev@gmail.com>
2022-11-24 19:17:06 +0300

    test1

---
+++ static/main.js
@@ -0,0 +1,79 @@
+
+    const Links = document.querySelectorAll(".nav__link")
+    console.log(Links)
+    Links.forEach((value, indexNav) => {
+      value.addEventListener("click", function() {
+        console.log(indexNav)
+        
+        const footerChar = document.querySelectorAll("#footer");
+        
+        footerChar.forEach((value, index1) => {
+           if (indexNav === index1) {
+            function scrollTo(element2) {
+              window.scroll({
+                left: 0,
+                top: element2.offsetTop,
+                behavior: 'smooth'
+              });
+             
+            };
+            scrollTo(value);
+           };
+           
+        });
+     
+      });
+     
+    });
+    
+    const Popup = document.querySelector(".popup");
+    const BurgerBtn = document.querySelector(".burger");
+    const BurgerItem = document.querySelectorAll(".burger__item");
+    const Nav = document.querySelector('.nav').cloneNode(1)
+    const popupInner = document.querySelector(".popup_inner")
+    const footerChar = document.querySelectorAll("#footer");
+
+    BurgerBtn.addEventListener("click", function() {
+	Popup.classList.toggle("visible-popup");
+
+    BurgerItem.forEach((item) => {
+       item.classList.toggle("burger__item--active_items")
+    });
+    Nav.classList.toggle("active")
+    popupInner.appendChild(Nav)
+    const navLinkChar = Array.from(document.querySelectorAll(".nav__link"))
+    const NavLinkCharSlice = navLinkChar.slice(7, 14)
+    NavLinkCharSlice.forEach((element, indexNav) => {
+        element.addEventListener("click", ()=> {
+            Popup.classList.remove("visible-popup");
+            BurgerItem.forEach((item) => {
+                item.classList.remove("burger__item--active_items")
+             });
+             Nav.classList.remove("active")
+             popupInner.appendChild(Nav)
+             
+             footerChar.forEach((value, index1) => {
+                if (indexNav === index1) {
+                 function scrollTo(element2) {
+                   window.scroll({
+                     left: 0,
+                     top: element2.offsetTop,
+                     behavior: 'smooth'
+                   });
+                  
+                 };
+                 scrollTo(value);
+                };
+                
+             });
+            
+        })
+    })
+    
+   
+    
+})
+    
+
+
+
